2cfe5020796059cd8777a0f1807e7b1bda080bec
Iftekharul <iftekhaulislam.cse@gamil.com>
2025-05-18 03:06:20 +0600

    update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-// export const API_URL = 'https://api.itsallaboutheoptions.com/api'
-// export const STRIPE_PUBLISHABLE_KEY = "pk_live_51OZxhhElQu0OoB1S9cw2Atn3bOn2eSafGFJhSbFUrE2xjkeTgEP6WsNhz2cC9KIQ9PXDxBhrWncAMbSnlguPOHds00XbSLrW2e"
+export const API_URL = 'https://api.itsallaboutheoptions.com/api'
+export const STRIPE_PUBLISHABLE_KEY = "pk_live_51OZxhhElQu0OoB1S9cw2Atn3bOn2eSafGFJhSbFUrE2xjkeTgEP6WsNhz2cC9KIQ9PXDxBhrWncAMbSnlguPOHds00XbSLrW2e"
 
-export const API_URL = 'https://allaboutheoptions.test/api'
-export const STRIPE_PUBLISHABLE_KEY = "pk_test_51RPMkFPIaOeAjfMJobISM72lzOLW6tYpHKP09BboJa1m8TLrHrrFBBA9qrFfJLh4BsnRGkzV1XXEtUORjFUCPirq00y5DE0dKD"
+// export const API_URL = 'https://allaboutheoptions.test/api'
+// export const STRIPE_PUBLISHABLE_KEY = "pk_test_51RPMkFPIaOeAjfMJobISM72lzOLW6tYpHKP09BboJa1m8TLrHrrFBBA9qrFfJLh4BsnRGkzV1XXEtUORjFUCPirq00y5DE0dKD"
 
 
